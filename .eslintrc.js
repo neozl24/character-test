fca7c7937cb7f1f466e1c11914f2325a6fc02914
zhongli <neozl24@hotmail.com>
2017-07-27 21:49:01 +0800

    initialize project

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// http://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // 默认配置来自于饿了么的eslint config
+  // https://github.com/ElemeFE/eslint-config-elemefe
+  extends: 'elemefe',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+  }
+}
